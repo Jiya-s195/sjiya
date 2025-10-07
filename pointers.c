e983ce568c6c5e5ee34ca44f98913174bc21366d
Jiya-s195 <073ahome@gmail.com>
2025-10-07 17:17:46 +0530

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+ int var=10;
+ int *ptr;
+ ptr = &var;
+ printf("Value of var:%d\n",var);
+ printf("Address of var:%d\n",&var);
+ printf("Value stored in ptr:%d\n",ptr);
+ printf("Value pointed by ptr:%d\n",*ptr);
+ printf("Address of pointer %u\n",&ptr);
+ return 0;
+}
